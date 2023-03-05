e74b5b20af721a35edbe6171c6204432de0c8a85
ameliagan <ameliagan@gsd.harvard.edu>
2023-03-04 21:43:00 -0500

    deploy box2

--- src/script.js
+++ src/script.js
@@ -1,17 +1,21 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {TransformControls} from 'three/addons/controls/TransformControls.js';
 import * as dat from 'lil-gui'
 import * as CANNON from 'cannon-es'
-import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 
-//object
+let cameraPersp, cameraOrtho, currentCamera;
+let scene, renderer, control, orbit;
+
+init();
+render();
+
 const gui = new dat.GUI()
-const Object = {}
-const objectsToUpdate = [];
 
+//object 
 Object.Box = () =>
 {
-    const box = Box(
+    Box(
         //box size
         1,
         1,
@@ -22,16 +26,8 @@ Object.Box = () =>
             y: 3,
             z: (Math.random() - 0.5) * 3
         }
-    );
-    Object.box = box; // add the mesh object to the object property
-    box.userData = { type: 'box', body: boxBody }; // set the type of the object and its Cannon.js body to the userData property of the mesh object
-    scene.add(box); // add the mesh object to the scene
-
-    transformControls.attach(box);
-    objectsToUpdate.push(transformControls); // add transform controls to objectsToUpdate array
-
+    )
 }
-
 gui.add(Object, 'Box')
 
 Object.Sphere = () =>
@@ -47,7 +43,6 @@ Object.Sphere = () =>
         }
     )
 }
-
 gui.add(Object, 'Sphere')
 
 
@@ -62,11 +57,8 @@ Object.reset = () =>
 
         // Remove mesh
         scene.remove(object.mesh)
-
-        //remove control widgets
-        transformControls.detach()
     }
-
+    
     objectsToUpdate.splice(0, objectsToUpdate.length)
 }
 gui.add(Object, 'reset')
@@ -78,88 +70,6 @@ gui.add(Object, 'reset')
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
-const scene = new THREE.Scene()
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(- 3, 3, 3)
-scene.add(camera)
-
-//orbit 
-const orbit = new OrbitControls(camera, renderer.domElement );
-orbit.update();
-orbit.addEventListener( 'change', renderer );
-
-// Add a flag to keep track of the state of the mouse drag transformation
-let enableMouseDrag = false;
-
-// transform 
-const transformControls = new TransformControls(camera, renderer.domElement);
-transformControls.mode = 'translate'; // set the mode to 'translate'
-scene.add(transformControls);
-
-transformControls.addEventListener('dragging-changed', function (event) {
-    orbit.enabled = !event.value; // disable orbit controls when dragging
-    enableMouseDrag = event.value; // set enableMouseDrag to true when dragging starts, false otherwise
-});
-
-transformControls.addEventListener('objectChange', function (event) {
-    if (enableMouseDrag && event.target.userData.type === 'box') { // check if the object is a box and dragging is enabled
-        event.target.userData.body.position.copy(event.target.position); // set the position of the box's Cannon.js body to the position of the transformControls object
-        event.target.userData.body.quaternion.copy(event.target.quaternion); // set the rotation of the box's Cannon.js body to the rotation of the transformControls object
-    }
-});
-
-
-/**
- * Sounds
- */
-const hitSound = new Audio('/sounds/hit.mp3')
-
-const playHitSound = (collision) =>
-{
-    const impactStrength = collision.contact.getImpactVelocityAlongNormal()
-
-    if(impactStrength > 1.5)
-    {
-        hitSound.volume = Math.random()
-        hitSound.currentTime = 0
-        hitSound.play()
-    }
-}
-
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
-
-const environmentMapTexture = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.png',
-])
 
 /**
  * Physics
@@ -181,12 +91,6 @@ const defaultContactMaterial = new CANNON.ContactMaterial(
 )
 world.defaultContactMaterial = defaultContactMaterial
 
-function render() {
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
-}
-
-
 // Floor
 const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
@@ -196,125 +100,120 @@ floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(- 1, 0, 0), Math.PI * 0.5)
 world.addBody(floorBody)
 
 
-//resize window
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-
 /**
- * Utils
+ * Sizes
  */
-
-// Create sphere
-const sphereGeometry = new THREE.SphereGeometry(4, 20, 20)
-const sphereMaterial = new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture,
-    envMapIntensity: 0.5
-});
-
-const Sphere = (radius, position) =>
-{
-    // Three.js mesh
-    const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
-    mesh.castShadow = true
-    mesh.scale.set(radius, radius, radius)
-    mesh.position.copy(position)
-    scene.add(mesh)
-
-    // Cannon.js body
-    const shape = new CANNON.Sphere(radius)
-
-    const body = new CANNON.Body({
-        mass: 1,
-        position: new CANNON.Vec3(0, 3, 0),
-        shape: shape,
-        material: defaultMaterial
-    })
-    body.position.copy(position)
-    body.addEventListener('collide', playHitSound)
-    world.addBody(body)
-
-    // Save in objects
-    objectsToUpdate.push({ mesh, body })
-
-    // Add transform control
-    const control = new THREE.TransformControls(camera, renderer.domElement);
-    control.attach(mesh);
-    scene.add(control);
-
-    return { mesh, body, control };
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-// Create box
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const boxMaterial = new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture,
-    envMapIntensity: 0.5
-})
+/**
+ * Sounds
+ */
+const hitSound = new Audio('/sounds/hit.mp3')
 
+const playHitSound = (collision) =>
+{
+    const impactStrength = collision.contact.getImpactVelocityAlongNormal()
 
-//box
-function Box(width, height, depth, position) {
-    // Three.js mesh
-    const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    const transformcontrol = new TransformControls(camera, renderer.domElement);
-    transformcontrol.attach(mesh);
-    mesh.scale.set(width, height, depth);
-    mesh.castShadow = true;
-    mesh.position.copy(position);
-    scene.add(mesh);
+    if(impactStrength > 1.5)
+    {
+        hitSound.volume = Math.random()
+        hitSound.currentTime = 0
+        hitSound.play()
+    }
+}
 
-    transformcontrol.addEventListener('dragging-changed', function (event) {
+function init() {
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const aspect = window.innerWidth / window.innerHeight;
+
+    cameraPersp = new THREE.PerspectiveCamera(50, aspect, 0.01, 30000);
+    cameraOrtho = new THREE.OrthographicCamera(-600 * aspect, 600 * aspect, 600, -600, 0.01, 30000);
+    currentCamera = cameraPersp;
+
+    currentCamera.position.set(1000, 500, 1000);
+    currentCamera.lookAt(0, 200, 0);
+
+    //grid
+    scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(100000, 100, 0x888888, 0x444444));
+
+    const light = new THREE.DirectionalLight(0xffffff, 2);
+    light.position.set(1, 1, 1);
+    scene.add(light);
+
+    const texture = new THREE.TextureLoader().load('concrete.jpg', render);
+    texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+
+    //box geometry 
+    const boxgeometry = new THREE.BoxGeometry(200, 200, 200);
+    const boxmaterial = new THREE.MeshLambertMaterial({
+        map: texture,
+        transparent: true
+    });
+
+    //box2 geometry
+    const boxgeometry2 = new THREE.BoxGeometry(200, 200, 200);
+    const boxmaterial2 = new THREE.MeshLambertMaterial({
+        map: texture,
+        transparent: true
+    });
+
+    orbit = new OrbitControls(currentCamera, renderer.domElement);
+    orbit.update();
+    orbit.addEventListener('change', render);
+
+    control = new TransformControls(currentCamera, renderer.domElement);
+    control.addEventListener('change', render);
+
+    control.addEventListener('dragging-changed', function (event) {
+
         orbit.enabled = !event.value;
+
     });
 
-    transformcontrol.attach(mesh);
-    scene.add(transformcontrol);
+    const boxmesh = new THREE.Mesh(boxgeometry, boxmaterial);
+    const boxmesh2 = new THREE.Mesh(boxgeometry2, boxmaterial2);
+    scene.add(boxmesh);
+    scene.add(boxmesh2);
 
-    transformcontrol.addEventListener('change', function () {
-        render();
-    });
+    control.attach(boxmesh);
+    control.attach(boxmesh2);
+    scene.add(control);
 
     window.addEventListener('resize', onWindowResize);
 
     window.addEventListener('keydown', function (event) {
+
         switch (event.keyCode) {
+
             case 81: // Q
-                transformcontrol.setSpace(transformcontrol.space === 'local' ? 'world' : 'local');
+                control.setSpace(control.space === 'local' ? 'world' : 'local');
                 break;
 
             case 16: // Shift
-                transformcontrol.setTranslationSnap(100);
-                transformcontrol.setRotationSnap(THREE.MathUtils.degToRad(15));
-                transformcontrol.setScaleSnap(0.25);
+                control.setTranslationSnap(100);
+                control.setRotationSnap(THREE.MathUtils.degToRad(15));
+                control.setScaleSnap(0.25);
                 break;
 
             case 87: // W
-                transformcontrol.setMode('translate');
+                control.setMode('translate');
                 break;
 
             case 69: // E
-                transformcontrol.setMode('rotate');
+                control.setMode('rotate');
                 break;
 
             case 82: // R
-                transformcontrol.setMode('scale');
+                control.setMode('scale');
                 break;
 
             case 67: // C
@@ -324,7 +223,7 @@ function Box(width, height, depth, position) {
                 currentCamera.position.copy(position);
 
                 orbit.object = currentCamera;
-                transformcontrol.camera = currentCamera;
+                control.camera = currentCamera;
 
                 currentCamera.lookAt(orbit.target.x, orbit.target.y, orbit.target.z);
                 onWindowResize();
@@ -345,178 +244,76 @@ function Box(width, height, depth, position) {
 
             case 187:
             case 107: // +, =, num+
-                transformcontrol.setSize(transformcontrol.size + 0.1);
+                control.setSize(control.size + 0.1);
                 break;
 
             case 189:
             case 109: // -, _, num-
-                transformcontrol.setSize(Math.max(transformcontrol.size - 0.1, 0.1));
+                control.setSize(Math.max(control.size - 0.1, 0.1));
                 break;
 
             case 88: // X
-                transformcontrol.showX = !transformcontrol.showX;
+                control.showX = !control.showX;
                 break;
 
             case 89: // Y
-                transformcontrol.showY = !transformcontrol.showY;
+                control.showY = !control.showY;
                 break;
 
             case 90: // Z
-                transformcontrol.showZ = !transformcontrol.showZ;
+                control.showZ = !control.showZ;
                 break;
 
             case 32: // Spacebar
-                transformcontrol.enabled = !transformcontrol.enabled;
+                control.enabled = !control.enabled;
                 break;
 
             case 27: // Esc
-                transformcontrol.reset();
+                control.reset();
                 break;
+
         }
+
     });
 
     window.addEventListener('keyup', function (event) {
+
         switch (event.keyCode) {
+
             case 16: // Shift
-                transformcontrol.setTranslationSnap(null);
-                transformcontrol.setRotationSnap(null);
-                transformcontrol.setScaleSnap(null);
+                control.setTranslationSnap(null);
+                control.setRotationSnap(null);
+                control.setScaleSnap(null);
                 break;
+
         }
+
     });
 
-    function onWindowResize() {
-        const aspect = window.innerWidth / window.innerHeight;
+}
 
-        cameraPersp.aspect = aspect;
-        cameraPersp.updateProjectionMatrix();
+function onWindowResize() {
 
-        cameraOrtho.left = cameraOrtho.bottom * aspect;
-        cameraOrtho.right = cameraOrtho.top * aspect;
-        cameraOrtho.updateProjectionMatrix();
+    const aspect = window.innerWidth / window.innerHeight;
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
+    cameraPersp.aspect = aspect;
+    cameraPersp.updateProjectionMatrix();
 
-        render();
-
-    }
-    // Cannon.js body
-    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
-
-    const body = new CANNON.Body({
-        mass: 1,
-        position: new CANNON.Vec3(position.x, position.y, position.z),
-        shape: shape,
-        material: defaultMaterial
-    })
-    body.addEventListener('collide', playHitSound)
-    world.addBody(body)
-
-    // Save in objects
-    objectsToUpdate.push({ mesh: mesh, body: body })
-
-    // Add mouse event listeners
-    let isDragging = false
-    let lastMousePosition = new THREE.Vector2()
-
-    canvas.addEventListener('mousedown', (event) => {
-        event.preventDefault()
-        if (enableMouseDrag) { // check if mouse drag is enabled
-            isDragging = true
-            lastMousePosition.set(event.clientX, event.clientY)
-            
-        }
-    })
-
-    canvas.addEventListener('mousemove', (event) => {
-        event.preventDefault()
-        if (isDragging && enableMouseDrag) { // check if mouse drag is enabled
-            const deltaX = event.clientX - lastMousePosition.x
-            const deltaY = event.clientY - lastMousePosition.y
+    cameraOrtho.left = cameraOrtho.bottom * aspect;
+    cameraOrtho.right = cameraOrtho.top * aspect;
+    cameraOrtho.updateProjectionMatrix();
     
-            // Move the box based on mouse x and y position
-            mesh.position.x += deltaX * 0.01
-            mesh.position.y -= deltaY * 0.01
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-            // Update the Cannon.js body position
-            body.position.set(mesh.position.x, mesh.position.y, mesh.position.z);
-    
-            lastMousePosition.set(event.clientX, event.clientY)
-            
-        }
-    })
-    
-    canvas.addEventListener('mouseup', () => {
-        isDragging = false
-    })
+    render();
 
-    return mesh;
-    
 }
 
 
-Box(1, 1, 1, { x: 0, y: 3, z: 0 })
 
-/**
- * Floor
- */
+function render() {
 
-//infinite plane
+    renderer.render(scene, currentCamera);
 
-const gridHelper = new THREE.GridHelper(200,50);
-scene.add(gridHelper);
-
-/**
- * Lights
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
-scene.add(ambientLight)
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
-directionalLight.shadow.camera.top = 7
-directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
-directionalLight.position.set(5, 5, 5)
-scene.add(directionalLight)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-
-/**
- * Animate
- */
-const clock = new THREE.Clock()
-let oldElapsedTime = 0
-
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - oldElapsedTime
-    oldElapsedTime = elapsedTime
-
-    // Update physics
-    world.step(1 / 60, deltaTime, 3)
-    
-    for(const object of objectsToUpdate)
-    {
-        object.mesh.position.copy(object.body.position)
-        object.mesh.quaternion.copy(object.body.quaternion)
-    }
-
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
 }
-
-tick()
